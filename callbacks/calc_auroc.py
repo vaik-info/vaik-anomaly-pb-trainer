5aa7b3f36736352659d21338c14d37d1ced29202
vaik-info <info@vaikobo.com>
2023-03-19 14:53:00 +0900

    first commit

--- callbacks/calc_auroc.py
+++ callbacks/calc_auroc.py
@@ -1,44 +1,20 @@
-import glob
-import os
-from PIL import Image
 from sklearn import metrics
 import numpy as np
 
-from data import ops
+def instance_auroc_mean(good_mse_array, anomaly_mse_array):
+    gt_labels = [True, ] * good_mse_array.shape[0]
+    gt_labels.extend([False, ] * anomaly_mse_array.shape[0])
+    mse_array = np.concatenate([good_mse_array, anomaly_mse_array], axis=0)
+    mse_mean_list = [np.mean(mse) for mse in mse_array]
+    return instance_auroc(gt_labels, mse_mean_list)
 
 
-def prepare_inf_gt_images(raw_image_dir_path, gt_image_dir_path, target_shape):
-    # read
-    raw_image_path_list = []
-    for file in ('*.jpg', '*.jpeg', '*.png', '*.JPG', '*.JPEG', '*.PNG'):
-        raw_image_path_list.extend(glob.glob(os.path.join(raw_image_dir_path, '**', f'{file}'), recursive=True))
-    raw_image_path_list = sorted(raw_image_path_list)
-
-    raw_image_list = []
-    gt_image_list = []
-    category_list = []
-    for raw_image_path in raw_image_path_list:
-        category = raw_image_path.split('/')[-2]
-        raw_image_list.append(ops.read_image(raw_image_path, target_shape)[0])
-        category_list.append(category)
-        if category != 'good':
-            gt_image_path_list = glob.glob(os.path.join(gt_image_dir_path, category, os.path.basename(raw_image_path).replace('.', '_mask.')))
-            gt_image_path = gt_image_path_list[0]
-            gt_image_list.append(ops.read_image(gt_image_path, (target_shape[0], target_shape[1]))[0])
-        else:
-            gt_image_list.append(np.zeros((raw_image_list[-1].shape[0], raw_image_list[-1].shape[1], 1), dtype=raw_image_list[-1].dtype))
-    return np.asarray(raw_image_list), np.asarray(gt_image_list), category_list
-
-def instance_auroc_mean(inf_raw_image_list, gt_image_list):
-    gt_labels = [np.max(gt_image) > 125 for gt_image in gt_image_list]
-    inf_raw_mean_list = [np.mean(inf_raw_image) for inf_raw_image in inf_raw_image_list]
-    return instance_auroc(gt_labels, inf_raw_mean_list)
-
-
-def instance_auroc_max(inf_raw_image_list, gt_image_list):
-    gt_labels = [np.max(gt_image) > 125 for gt_image in gt_image_list]
-    inf_raw_max_list = [np.max(inf_raw_image) for inf_raw_image in inf_raw_image_list]
-    return instance_auroc(gt_labels, inf_raw_max_list)
+def instance_auroc_max(good_mse_array, anomaly_mse_array):
+    gt_labels = [True, ] * good_mse_array.shape[0]
+    gt_labels.extend([False, ] * anomaly_mse_array.shape[0])
+    mse_array = np.concatenate([good_mse_array, anomaly_mse_array], axis=0)
+    mse_mean_list = [np.max(mse) for mse in mse_array]
+    return instance_auroc(gt_labels, mse_mean_list)
 
 
 def instance_auroc(gt_labels, inf_raw_list):
@@ -49,41 +25,3 @@ def instance_auroc(gt_labels, inf_raw_list):
         gt_labels, inf_raw_list
     )
     return auroc_metric, {'fpr': fpr, 'tpr': tpr, 'thresholds': thresholds}
-
-
-def full_pixel_auroc(inf_raw_image_list, gt_image_list):
-    gt_image_array = (np.asarray(gt_image_list) > 125).flatten().astype(np.uint8)
-    inf_raw_image_array = np.asarray(inf_raw_image_list).flatten()
-    fpr, tpr, thresholds = metrics.roc_curve(
-        gt_image_array, inf_raw_image_array
-    )
-    auroc_metric = metrics.roc_auc_score(
-        gt_image_array, inf_raw_image_array
-    )
-    return auroc_metric, {'fpr': fpr, 'tpr': tpr, 'thresholds': thresholds}
-
-
-def anomaly_pixel_auroc(inf_raw_image_list, gt_image_list):
-    gt_labels = [np.max(gt_image) > 125 for gt_image in gt_image_list]
-    gt_anomaly_indexes = np.asarray(gt_labels) == True
-    gt_image_list = [gt_image for index, gt_image in enumerate(gt_image_list) if gt_anomaly_indexes[index]]
-    inf_raw_image_list = [inf_raw_image for index, inf_raw_image in enumerate(inf_raw_image_list) if
-                          gt_anomaly_indexes[index]]
-    return full_pixel_auroc(inf_raw_image_list, gt_image_list)
-
-
-def anomaly_detail_pixel_auroc(inf_raw_image_list, gt_image_list, inf_image_path_list):
-    gt_labels = [np.max(gt_image) > 125 for gt_image in gt_image_list]
-    gt_anomaly_indexes = np.asarray(gt_labels) == True
-    gt_image_list = [gt_image for index, gt_image in enumerate(gt_image_list) if gt_anomaly_indexes[index]]
-    inf_raw_image_list = [inf_raw_image for index, inf_raw_image in enumerate(inf_raw_image_list) if
-                          gt_anomaly_indexes[index]]
-    inf_image_path_list = [inf_image_path for index, inf_image_path in enumerate(inf_image_path_list) if
-                           gt_anomaly_indexes[index]]
-
-    auroc_metric_list, detail_list = [], []
-    for inf_raw_image, gt_image, inf_image_path in zip(inf_raw_image_list, gt_image_list, inf_image_path_list):
-        auroc_metric, detail_dict = full_pixel_auroc([inf_raw_image], [gt_image])
-        auroc_metric_list.append((inf_image_path, auroc_metric))
-        detail_list.append(detail_dict)
-    return auroc_metric_list, detail_list
--- train.py
+++ train.py
@@ -43,9 +43,8 @@ def train(train_image_dir_path, test_good_image_dir_path, test_anomaly_image_dir
     ## valid anomaly
     TestAnomalyDataset = type(f'TestAnomalyDataset', (ae_dataset.AEDataset,), dict())
     valid_anomaly_dataset = TestAnomalyDataset(test_anomaly_image_dir_path, input_shape)
-    valid_anomaly_data= next(iter(valid_anomaly_dataset.padded_batch(batch_size=test_max_sample, padding_values=(
+    valid_anomaly_data = next(iter(valid_anomaly_dataset.padded_batch(batch_size=test_max_sample, padding_values=(
         tf.constant(0, dtype=tf.uint8), tf.constant(0, dtype=tf.uint8)))))
-
 
     # Prepare Model
     ## encoder
@@ -63,7 +62,8 @@ def train(train_image_dir_path, test_good_image_dir_path, test_anomaly_image_dir
     encoder_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
     decoder_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
 
-    save_model_dir_path = os.path.join(output_dir_path, f'{datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d-%H-%M-%S")}')
+    save_model_dir_path = os.path.join(output_dir_path,
+                                       f'{datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d-%H-%M-%S")}')
 
     # train
     @tf.function
@@ -91,8 +91,8 @@ def train(train_image_dir_path, test_good_image_dir_path, test_anomaly_image_dir
         generated_images = decoder_model(latent, training=True)
 
         loss = vae_loss.vae_loss(output_images, generated_images, mean, log_var)
-        mse = tf.keras.metrics.mse(tf.cast(output_images, tf.float32) / 255., generated_images)
-        return tf.reduce_mean(loss), tf.reduce_mean(mse), generated_images
+        mse_array = tf.math.squared_difference(generated_images, tf.cast(output_images, tf.float32) / 255.)
+        return tf.reduce_mean(loss), tf.reduce_mean(mse_array), generated_images, mse_array
 
     # test
     @tf.function
@@ -117,18 +117,37 @@ def train(train_image_dir_path, test_good_image_dir_path, test_anomaly_image_dir
                 monitor_tqdm.set_postfix(loss=float(np.mean(train_loss_list)), mse=float(np.mean(train_mse_list)))
 
             # valid
-            train_loss, train_mse, train_generated_images = test_step(train_good_data[0], train_good_data[1])
-            val_loss, val_mse, val_generated_images = test_step(valid_good_data[0], valid_good_data[1])
-            print(f'loss:{float(train_loss):.4f}, val_loss:{float(val_loss):.4f}, mse:{float(train_mse):.4f}, val_mse:{float(val_mse):.4f}')
+            train_loss, train_mse, train_generated_images, train_mse_array = test_step(train_good_data[0],
+                                                                                       train_good_data[1])
+            val_loss, val_mse, val_generated_images, val_mse_array = test_step(valid_good_data[0], valid_good_data[1])
+            val_anomaly_loss, val_anomaly_mse, val_anomaly_generated_images, val_anomaly_mse_array = test_step(
+                valid_anomaly_data[0],
+                valid_anomaly_data[1])
 
+            val_auroc_mean, val_auroc_mean_detail = calc_auroc.instance_auroc_mean(val_mse_array, val_anomaly_mse_array)
+            val_auroc_max, val_auroc_max_detail = calc_auroc.instance_auroc_max(val_mse_array, val_anomaly_mse_array)
+
+            print(f'loss:{float(train_loss):.4f}, val_loss:{float(val_loss):.4f}, val_anomaly_loss:{float(val_anomaly_loss):.4f}, '
+                f'mse:{float(train_mse):.4f}, val_mse:{float(val_mse):.4f}, val_anomaly_mse:{float(val_anomaly_mse):.4f}',
+                f'val_auroc_mean:{float(val_auroc_mean):.4f}, val_auroc_max:{float(val_auroc_max):.4f}')
+
+            # save model
+            save_model_sub_dir_path = os.path.join(save_model_dir_path,
+                                                   f'epoch-{epoch:04d}_steps-{step_size}_batch-{batch_size}_'
+                                                   f'loss-{float(train_loss):.4f}_val_loss-{float(val_loss):.4f}_val_anomaly_loss-{float(val_anomaly_loss):.4f}_'
+                                                   f'mse-{float(train_mse):.4f}-val_mse-{float(val_mse):.4f}_val_anomaly_mse-{float(val_anomaly_mse):.4f}_'
+                                                   f'val_auroc_mean-{float(val_auroc_mean)}_val_auroc_max-{float(val_auroc_max)}')
+            os.makedirs(save_model_sub_dir_path, exist_ok=True)
+            all_model.save(os.path.join(save_model_sub_dir_path, 'all_model'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='train pb')
     parser.add_argument('--train_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/train/good')
     parser.add_argument('--test_good_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/valid/good')
-    parser.add_argument('--test_anomaly_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/valid/anomaly')
+    parser.add_argument('--test_anomaly_image_dir_path', type=str,
+                        default='~/.vaik-mnist-anomaly-dataset/valid/anomaly')
     parser.add_argument('--epoch_size', type=int, default=1000)
-    parser.add_argument('--step_size', type=int, default=200)
+    parser.add_argument('--step_size', type=int, default=2000)
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--image_height', type=int, default=224)
     parser.add_argument('--image_width', type=int, default=224)
