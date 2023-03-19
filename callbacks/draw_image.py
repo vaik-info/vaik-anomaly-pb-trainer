96479135f99732035a3262f5f73affea0fd21183
vaik-info <info@vaikobo.com>
2023-03-19 15:41:20 +0900

    first commit

--- callbacks/draw_image.py
+++ callbacks/draw_image.py
@@ -3,22 +3,20 @@ import numpy as np
 import os
 
 
-def draw_image(y_true, y_pred, output_dir_path, auroc_valid_category_list=None, rescale=255.):
-    os.makedirs(output_dir_path, exist_ok=True)
-    inf_raw_mean_list = [np.mean(np.abs(raw_image/rescale-inf_raw_image)) for raw_image, inf_raw_image in zip(y_true, y_pred)]
-    for index in range(y_true.shape[0]):
-        ground_truth_image, generated_image = y_true[index], y_pred[index]
-        ground_truth_image = Image.fromarray(ground_truth_image)
-        if auroc_valid_category_list is None:
-            ground_truth_image.save(os.path.join(output_dir_path, f'{inf_raw_mean_list[index]:.4f}_{index:04d}_gt.png'))
-        else:
-            ground_truth_image.save(os.path.join(output_dir_path, f'{inf_raw_mean_list[index]:.4f}_{index:04d}_{auroc_valid_category_list[index]}_gt.png'))
+def draw_image(good_inf_images, good_mses, anomaly_inf_images, anomaly_mses, output_dir_path, rescale=255.):
+    good_mse_mean_list = [np.mean(good_mse) for good_mse in good_mses]
+    output_sub_dir_path = os.path.join(output_dir_path, 'good')
+    os.makedirs(output_sub_dir_path, exist_ok=True)
+    for good_index, good_inf_image in enumerate(good_inf_images):
+        good_inf_image = np.clip(good_inf_image * rescale, 0, 255).astype(np.uint8)
+        good_inf_image = Image.fromarray(good_inf_image)
+        good_inf_image.save(os.path.join(output_sub_dir_path, f'{good_mse_mean_list[good_index]:.4f}.png'))
 
-        generated_image = np.clip(generated_image*rescale, 0., 255.).astype(np.uint8)
-        generated_image = Image.fromarray(generated_image)
-        if auroc_valid_category_list is None:
-            generated_image.save(os.path.join(output_dir_path, f'{inf_raw_mean_list[index]:.4f}_{index:04d}_pred.png'))
-        else:
-            generated_image.save(os.path.join(output_dir_path, f'{inf_raw_mean_list[index]:.4f}_{index:04d}_{auroc_valid_category_list[index]}_pred.png'))
-
+    anomaly_mse_mean_list = [np.mean(anomaly_mse) for anomaly_mse in anomaly_mses]
+    output_sub_dir_path = os.path.join(output_dir_path, 'anomaly')
+    os.makedirs(output_sub_dir_path, exist_ok=True)
+    for anomaly_index, anomaly_inf_image in enumerate(anomaly_inf_images):
+        anomaly_inf_image = np.clip(anomaly_inf_image * rescale, 0, 255).astype(np.uint8)
+        anomaly_inf_image = Image.fromarray(anomaly_inf_image)
+        anomaly_inf_image.save(os.path.join(output_sub_dir_path, f'{anomaly_mse_mean_list[anomaly_index]:.4f}.png'))
 
