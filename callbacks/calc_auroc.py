5031d0dfcbd20a162f6a67771c5beb660c5d4344
vaik-info <info@vaikobo.com>
2023-03-19 15:53:24 +0900

    first commit

--- callbacks/calc_auroc.py
+++ callbacks/calc_auroc.py
@@ -2,16 +2,16 @@ from sklearn import metrics
 import numpy as np
 
 def instance_auroc_mean(good_mse_array, anomaly_mse_array):
-    gt_labels = [True, ] * good_mse_array.shape[0]
-    gt_labels.extend([False, ] * anomaly_mse_array.shape[0])
+    gt_labels = [False, ] * good_mse_array.shape[0]
+    gt_labels.extend([True, ] * anomaly_mse_array.shape[0])
     mse_array = np.concatenate([good_mse_array, anomaly_mse_array], axis=0)
     mse_mean_list = [np.mean(mse) for mse in mse_array]
     return instance_auroc(gt_labels, mse_mean_list)
 
 
 def instance_auroc_max(good_mse_array, anomaly_mse_array):
-    gt_labels = [True, ] * good_mse_array.shape[0]
-    gt_labels.extend([False, ] * anomaly_mse_array.shape[0])
+    gt_labels = [False, ] * good_mse_array.shape[0]
+    gt_labels.extend([True, ] * anomaly_mse_array.shape[0])
     mse_array = np.concatenate([good_mse_array, anomaly_mse_array], axis=0)
     mse_mean_list = [np.max(mse) for mse in mse_array]
     return instance_auroc(gt_labels, mse_mean_list)
