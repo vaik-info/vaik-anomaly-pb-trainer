5623de4e6698b39a3e35fa017d6ede78ffc230d3
vaik-info <info@vaikobo.com>
2023-03-19 14:11:37 +0900

    first commit

--- data/ae_dataset.py
+++ data/ae_dataset.py
@@ -2,23 +2,18 @@ import glob
 import os
 import random
 import tensorflow as tf
-import numpy as np
 
 from data import ops
 
 
-class AnomalyDataset:
+class AEDataset:
     image_path_list = None
     image_shape = None
     output_signature = None
-    transform = None
-    noize_transform=None
 
-    def __new__(cls, input_dir_path, image_shape=(224, 224, 1), transform=None, noize_transform=None):
+    def __new__(cls, input_dir_path, image_shape=(224, 224, 3)):
         cls.image_shape = image_shape
         cls.image_path_list = cls.__prepare_image_path_list(input_dir_path, image_shape)
-        cls.transform = transform
-        cls.noize_transform = noize_transform
         cls.output_signature = (
             tf.TensorSpec(name=f'input_image', shape=image_shape, dtype=tf.uint8),
             tf.TensorSpec(name=f'output_image', shape=image_shape, dtype=tf.uint8)
@@ -36,21 +31,12 @@ class AnomalyDataset:
             image_path_list.extend(glob.glob(os.path.join(input_dir_path, '**', f'{file}'), recursive=True))
         image_list = []
         for image_path in image_path_list:
-            input_image, _, _ = ops.read_image(image_path, image_shape)
-            image_list.append(input_image)
+            image_list.append(image_path)
         return image_list
 
     @classmethod
     def _generator(cls):
         while True:
-            image = random.choice(cls.image_path_list)
-            if cls.transform is None:
-                image = tf.convert_to_tensor(image.astype(np.uint8))
-            else:
-                image = cls.transform(image=image)['image']
-
-            if cls.noize_transform is None:
-                noize_image = image
-            else:
-                noize_image = cls.noize_transform(image=image)['image']
-            yield tf.convert_to_tensor(noize_image), tf.convert_to_tensor(image)
+            input_image = ops.read_image(random.choice(cls.image_path_list), cls.image_shape)
+            input_image = tf.convert_to_tensor(input_image)
+            yield input_image, input_image
--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import pytz
 import tensorflow as tf
 import numpy as np
 import tqdm
-import cv2
 
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
@@ -17,40 +16,36 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-from data import anomaly_dataset
-import albumentations as A
+from data import ae_dataset
 from models import cvae_encoder, cvae_sampler, cvae_decoder
 from losses import vae_loss
 from callbacks import draw_image, calc_auroc
 
 
-def train(train_image_dir_path, test_image_dir_path,
-          auroc_valid_raw_image_dir_path, auroc_valid_gt_image_dir_path,
-          epoch_size, step_size, batch_size, image_height, image_width,
-          latent_dim,
-          test_max_sample, output_dir_path):
+def train(train_image_dir_path, test_good_image_dir_path, test_anomaly_image_dir_path,
+          epoch_size, step_size, batch_size, image_height, image_width, latent_dim, test_max_sample, output_dir_path):
     input_shape = (image_height, image_width, 3)
 
-    # Prepare Train Dataset
-    transform = A.Compose([
-        A.ShiftScaleRotate(always_apply=True)
-    ])
     ## train
-    TrainDataset = type(f'TrainDataset', (anomaly_dataset.AnomalyDataset,), dict())
-    train_dataset = TrainDataset(train_image_dir_path, input_shape, transform)
+    TrainDataset = type(f'TrainDataset', (ae_dataset.AEDataset,), dict())
+    train_dataset = TrainDataset(train_image_dir_path, input_shape)
+    train_good_data = next(iter(train_dataset.padded_batch(batch_size=test_max_sample, padding_values=(
+        tf.constant(0, dtype=tf.uint8), tf.constant(0, dtype=tf.uint8)))))
     train_dataset = iter(train_dataset.padded_batch(batch_size=batch_size, padding_values=(
         tf.constant(0, dtype=tf.uint8), tf.constant(0, dtype=tf.uint8))))
 
-    ## valid
-    TestDataset = type(f'TestDataset', (anomaly_dataset.AnomalyDataset,), dict())
-    valid_dataset = TestDataset(test_image_dir_path, input_shape)
-    valid_dataset = next(iter(valid_dataset.padded_batch(batch_size=test_max_sample, padding_values=(
+    ## valid good
+    TestGoodDataset = type(f'TestGoodDataset', (ae_dataset.AEDataset,), dict())
+    valid_good_dataset = TestGoodDataset(test_good_image_dir_path, input_shape)
+    valid_good_data = next(iter(valid_good_dataset.padded_batch(batch_size=test_max_sample, padding_values=(
         tf.constant(0, dtype=tf.uint8), tf.constant(0, dtype=tf.uint8)))))
 
-    # Prepare Auroc Dataset
-    ## valid
-    auroc_valid_raw_image_array, auroc_valid_gt_image_array, auroc_valid_category_list = calc_auroc.prepare_inf_gt_images(
-        auroc_valid_raw_image_dir_path, auroc_valid_gt_image_dir_path, input_shape)
+    ## valid anomaly
+    TestAnomalyDataset = type(f'TestAnomalyDataset', (ae_dataset.AEDataset,), dict())
+    valid_anomaly_dataset = TestAnomalyDataset(test_anomaly_image_dir_path, input_shape)
+    valid_anomaly_data= next(iter(valid_anomaly_dataset.padded_batch(batch_size=test_max_sample, padding_values=(
+        tf.constant(0, dtype=tf.uint8), tf.constant(0, dtype=tf.uint8)))))
+
 
     # Prepare Model
     ## encoder
@@ -68,8 +63,7 @@ def train(train_image_dir_path, test_image_dir_path,
     encoder_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
     decoder_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
 
-    save_model_dir_path = os.path.join(output_dir_path,
-                                       f'{datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d-%H-%M-%S")}')
+    save_model_dir_path = os.path.join(output_dir_path, f'{datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d-%H-%M-%S")}')
 
     # train
     @tf.function
@@ -121,48 +115,18 @@ def train(train_image_dir_path, test_image_dir_path,
                 train_loss_list.append(train_loss)
                 train_mse_list.append(train_mse)
                 monitor_tqdm.set_postfix(loss=float(np.mean(train_loss_list)), mse=float(np.mean(train_mse_list)))
+
             # valid
-            val_loss, val_mse, val_generated_images = test_step(valid_dataset[0], valid_dataset[1])
-            print(f'val_loss:{float(val_loss):.4f}, val_mse:{float(val_mse):.4f}')
-
-            # calc auroc
-            auroc_valid_inf_image_array = test_auroc_step(auroc_valid_raw_image_array)
-            valid_instance_auroc, _ = calc_auroc.instance_auroc_mean(auroc_valid_inf_image_array, auroc_valid_gt_image_array)
-            valid_mean_instance_auroc, _ = calc_auroc.instance_auroc_mean(auroc_valid_inf_image_array, auroc_valid_gt_image_array)
-            valid_max_instance_auroc, _ = calc_auroc.instance_auroc_max(auroc_valid_inf_image_array, auroc_valid_gt_image_array)
-            valid_full_pixel_instance_auroc, _ = calc_auroc.full_pixel_auroc(auroc_valid_inf_image_array, auroc_valid_gt_image_array)
-            valid_anomaly_pixel_instance_auroc, _ = calc_auroc.anomaly_pixel_auroc(auroc_valid_inf_image_array, auroc_valid_gt_image_array)
-
-            print(f'valid_instance_auroc(mean): {valid_mean_instance_auroc:.4f}')
-            print(f'valid_instance_auroc(max): {valid_max_instance_auroc:.4f}')
-            print(f'valid_full_pixel_auroc: {valid_full_pixel_instance_auroc:.4f}')
-            print(f'valid_anomaly_pixel_auroc: {valid_anomaly_pixel_instance_auroc:.4f}')
-
-            # draw image
-            save_model_sub_dir_path = os.path.join(save_model_dir_path,
-                                                   f'epoch-{epoch:04d}_steps-{step_size}_batch-{batch_size}_loss-{float(np.mean(train_loss_list)):.4f}_val_loss-{val_loss:.4f}_mses-{float(np.mean(train_mse_list)):.4f}_val_mse-{val_mse:.4f}_valid_full_pixel_auroc-{valid_full_pixel_instance_auroc:.4f}')
-            save_model_train_sub_dir_path = os.path.join(save_model_sub_dir_path, 'train_generated_images')
-            draw_image.draw_image(image_batch[1].numpy(), train_generated_images.numpy(), save_model_train_sub_dir_path)
-
-            save_model_val_sub_dir_path = os.path.join(save_model_sub_dir_path, 'val_generated_images')
-            draw_image.draw_image(valid_dataset[1].numpy(), val_generated_images.numpy(), save_model_val_sub_dir_path, auroc_valid_category_list)
-
-
-            _, _, auroc_valid_inf_image_array = test_step(auroc_valid_raw_image_array, auroc_valid_raw_image_array)
-            save_model_val_anomaly_sub_dir_path = os.path.join(save_model_sub_dir_path, 'val_anomaly_generated_images')
-            draw_image.draw_image(auroc_valid_raw_image_array, auroc_valid_inf_image_array.numpy(), save_model_val_anomaly_sub_dir_path)
-
-            # save model
-            all_model.save(os.path.join(save_model_sub_dir_path, 'all_model'))
+            train_loss, train_mse, train_generated_images = test_step(train_good_data[0], train_good_data[1])
+            val_loss, val_mse, val_generated_images = test_step(valid_good_data[0], valid_good_data[1])
+            print(f'loss:{float(train_loss):.4f}, val_loss:{float(val_loss):.4f}, mse:{float(train_mse):.4f}, val_mse:{float(val_mse):.4f}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='train pb')
-    parser.add_argument('--train_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/train/raw/good')
-    parser.add_argument('--test_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/valid/raw/good')
-    parser.add_argument('--auroc_valid_raw_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/valid/raw')
-    parser.add_argument('--auroc_valid_gt_image_dir_path', type=str,
-                        default='~/.vaik-mnist-anomaly-dataset/valid/ground_truth')
+    parser.add_argument('--train_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/train/good')
+    parser.add_argument('--test_good_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/valid/good')
+    parser.add_argument('--test_anomaly_image_dir_path', type=str, default='~/.vaik-mnist-anomaly-dataset/valid/anomaly')
     parser.add_argument('--epoch_size', type=int, default=1000)
     parser.add_argument('--step_size', type=int, default=200)
     parser.add_argument('--batch_size', type=int, default=32)
@@ -174,14 +138,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     args.train_image_dir_path = os.path.expanduser(args.train_image_dir_path)
-    args.auroc_valid_raw_image_dir_path = os.path.expanduser(args.auroc_valid_raw_image_dir_path)
-    args.auroc_valid_gt_image_dir_path = os.path.expanduser(args.auroc_valid_gt_image_dir_path)
-    args.test_image_dir_path = os.path.expanduser(args.test_image_dir_path)
+    args.test_good_image_dir_path = os.path.expanduser(args.test_good_image_dir_path)
+    args.test_anomaly_image_dir_path = os.path.expanduser(args.test_anomaly_image_dir_path)
     args.output_dir_path = os.path.expanduser(args.output_dir_path)
 
     os.makedirs(os.path.dirname(args.output_dir_path), exist_ok=True)
 
-    train(args.train_image_dir_path, args.test_image_dir_path,
-          args.auroc_valid_raw_image_dir_path, args.auroc_valid_gt_image_dir_path,
+    train(args.train_image_dir_path, args.test_good_image_dir_path, args.test_anomaly_image_dir_path,
           args.epoch_size, args.step_size, args.batch_size,
           args.image_height, args.image_width, args.latent_dim, args.test_max_sample, args.output_dir_path)
